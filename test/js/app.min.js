329c727763fbdfa45d587b2694f42e898dc25d62
John Doe <zhenua.pishelevsky@gmail.com>
2021-11-15 22:23:31 +0300

    го

---
+++ test/js/app.min.js
@@ -0,0 +1,49 @@
+
+/* Анимация */
+
+		 function onEntry(entry) {
+			entry.forEach(change => {
+			  if (change.isIntersecting) {
+				change.target.classList.add('element-show');} else {
+					change.target.classList.remove('element-show')
+				}
+			});
+		 }
+		 
+		 let options = {
+			threshold: [0.5] };
+		 let observer = new IntersectionObserver(onEntry, options);
+		 let elements = document.querySelectorAll('.element_animation');
+		 
+		 for (let elm of elements) {
+			observer.observe(elm);
+		 };
+		
+/* Анимация */
+
+
+/* Бургер */
+
+$('.burger').on('click', () => {
+   $('.burger').toggleClass('active');
+   $('.mobile nav').toggleClass('open');
+   $('.mobile__nav').toggleClass('show');
+	$('body').toggleClass('_lock')
+});
+
+
+/* Бургер */
+
+
+/* AOS animate*/
+
+AOS.init({
+	offset: 300,
+	duration: 300,
+	easing: "ease-in-sine",
+	delay: 100,
+	once:false,
+	placement:"top-top",
+});
+
+/* AOS animate*/
